9a2931ea3afca3ec6003f0ef230faf0347cb2855
Kim Fehrs <kfehrs@thoughtworks.com>
2018-12-28 00:26:14 +0100

    Add Diceboard
    TODP:
    - add multiplayer/ simultanious
    - responsive design for mobile
    - Erase Button/Undo
    - Lock rows for other players/ Show who locked the row
    - Winning symbol after locking 2 rows
    - Fail throws
    - Show board of other players
    - Hide score for other players
    - Chat

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React, {Component} from 'react';
 import './App.css';
 import {QuinxBoard} from './QuinxBoard';
+import {Button} from 'react-bootstrap';
 import {BootstrapStyled} from "./BootstrapStyled";
+
+const DiceBoard = (props) => {
+    return (<div>
+        <button disabled style={{backgroundColor:'WHITE'}}>{props.white1}</button>
+        <button disabled style={{backgroundColor:'WHITE'}}>{props.white2}</button>
+        <button disabled style={{backgroundColor:'RED'}}>{props.red}</button>
+        <button disabled style={{backgroundColor:'YELLOW'}}>{props.yellow}</button>
+        <button disabled style={{backgroundColor:'GREEN', color:'WHITE'}}>{props.green}</button>
+        <button disabled style={{backgroundColor:'BLUE', color:'WHITE'}}>{props.blue}</button>
+    </div>)
+};
 
 class App extends Component {
     state = {
@@ -11,7 +23,13 @@ class App extends Component {
             YELLOW: [],
             GREEN: [],
             lockedRows: []
-        }
+        },
+        white1:1,
+        white2:2,
+        red:2,
+        yellow:2,
+        green:2,
+        blue:2,
     };
 
     addToGamecard = (digit, color) => (event) => {
@@ -42,6 +60,24 @@ class App extends Component {
                                 addToGamecard={this.addToGamecard}
                                 lockRow={this.lockRow}
                     />
+                    <DiceBoard
+                        white1={this.state.white1}
+                        white2={this.state.white2}
+                        red={this.state.red}
+                        yellow={this.state.yellow}
+                        green={this.state.green}
+                        blue={this.state.blue}
+                    />
+                    <Button onClick={()=>{
+                        let diceRolls = {};
+                        diceRolls['white1'] = Math.floor(Math.random() * 6) + 1;
+                        diceRolls['white2'] = Math.floor(Math.random() * 6) + 1;
+                        diceRolls['red'] = Math.floor(Math.random() * 6) + 1;
+                        diceRolls['yellow'] = Math.floor(Math.random() * 6) + 1;
+                        diceRolls['green'] = Math.floor(Math.random() * 6) + 1;
+                        diceRolls['blue'] = Math.floor(Math.random() * 6) + 1;
+                        this.setState(diceRolls);
+                    }}>Roll Dice</Button>
                 </div>
             </div>
         );
